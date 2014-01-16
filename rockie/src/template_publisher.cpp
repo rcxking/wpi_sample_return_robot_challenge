de5b9bd19b8c6772af5d475a755d604a820805d2
carser <carser@minerva.(none)>
2014-01-16 13:55:46 -0500

    A file structure for ROS control of rover.

---
+++ rockie/src/template_publisher.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "publisher_node");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Rate loop_rate(10);
+
+  int count = 0; 	// A count of how many messages have been sent. 
+  while (ros::ok())
+  {
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+    ROS_INFO("%s", msg.data.c_str());
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
+
+
