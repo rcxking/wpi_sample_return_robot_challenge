748bd9f07d373d304dbcfdc34b95d2969a4afda2
will <will@ubuntu.(none)>
2014-03-11 10:43:09 -0400

    reverted landscape.world

--- rockie_code/src/tf_robot/src/tf_broadcaster.cpp
+++ rockie_code/src/tf_robot/src/tf_broadcaster.cpp
@@ -2,18 +2,25 @@
 #include <tf/transform_broadcaster.h>
 
 int main(int argc, char** argv){
-	ros::init(argc, argv, "robot_tf_publisher");
-	ros::NodeHandle n;
+  ros::init(argc, argv, "robot_tf_publisher");
+  ros::NodeHandle n;
 
-	ros::Rate r(100);
+  ros::Rate r(100);
 
-	tf::TransformBroadcaster broadcaster;
+  tf::TransformBroadcaster broadcaster;
 
-	while(n.ok()){
-		broadcaster.sendTransform(
-				tf::StampedTransform(
-					tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
-					ros::Time::now(),"base_link", "base_laser"));
-		r.sleep();
-	}
+  while(n.ok()){
+    broadcaster.sendTransform(
+        tf::StampedTransform(
+          tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
+          ros::Time::now(),"base_link", "camera_link"));
+
+    broadcaster.sendTransform(
+        tf::StampedTransform(
+          tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
+          ros::Time::now(),"odom", "map"));
+
+
+    r.sleep();
+  }
 }
