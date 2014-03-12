40db3a2933d6d5a7a699e3b484e07db7d61edde7
will <will@ubuntu.(none)>
2014-03-11 20:10:57 -0400

    spin camera and startup rviz in launch

--- gazebo/spin_robot.sh
+++ gazebo/spin_robot.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-rosservice call /gazebo/apply_joint_effort '{joint_name: "back_left_joint", effort: 7.0, start_time: {secs: 0, nsecs: 0}, duration: {secs: 10, nsecs: 0}}'
-
-rosservice call /gazebo/apply_joint_effort '{joint_name: "back_right_joint", effort: -7.0, start_time: {secs: 0, nsecs: 0}, duration: {secs: 10, nsecs: 0}}'
-
-rosservice call /gazebo/apply_joint_effort '{joint_name: "front_left_joint", effort: 7.0, start_time: {secs: 0, nsecs: 0}, duration: {secs: 10, nsecs: 0}}'
-
-rosservice call /gazebo/apply_joint_effort '{joint_name: "front_right_joint", effort: -7.0, start_time: {secs: 0, nsecs: 0}, duration: {secs: 10, nsecs: 0}}'
+rosservice call /gazebo/apply_joint_effort '{joint_name: "stereo_camera_joint", effort: -0.1, start_time: {secs: 0, nsecs: 0}, duration: {secs: 2, nsecs: 0}}'
 
 
