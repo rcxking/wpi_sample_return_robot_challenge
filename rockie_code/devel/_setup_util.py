424c2c49adbe490d96bb7e18f4ad6ba8743a160e
will <will@ubuntu.(none)>
2014-03-11 21:16:04 -0400

    added fovis packages to contrast with viso2_ros

--- rockie_code/devel/_setup_util.py
+++ rockie_code/devel/_setup_util.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/opt/ros/hydro'.split(';')
+        CMAKE_PREFIX_PATH = '/home/will/catkin_make/devel;/opt/ros/hydro'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
