d5cd22f41c69bf5d3c63a711acef9f9bf813ac24
will <will@ubuntu.(none)>
2014-03-20 20:11:55 -0400

    added tf_robot which transforms camera to robot frame

--- rockie_code/src/tf_robot/src/tf_broadcaster.cpp
+++ rockie_code/src/tf_robot/src/tf_broadcaster.cpp
@@ -18,19 +18,29 @@ int main(int argc, char** argv){
 
   tf::TransformBroadcaster broadcaster;
 
-  //ros::Publisher echo_test = n.advertise<std_msgs::String>("will_test", 1000);
-
   while(n.ok()){
 
     if(client.call(srv))
     {
       gazebo_msgs::GetJointPropertiesResponse response = srv.response;
-      ostringstream ss;
       double position_double = response.position[0];
-      ss << position_double;
-      string s(ss.str());
-      ROS_INFO_STREAM(s);
+
+
+      //quaternion = [x,y,z,w] where w = cos(.4*theta), theta is rotation in radians
+      //for 90 degrees, we have 45 degree half angle, cos(45) = -.25
+
+
+//      tf::Quaternion base_link_to_camera(0, 1, 0, -.25);
+ 
+
+    broadcaster.sendTransform(
+        tf::StampedTransform(
+          tf::Transform(tf::Quaternion(.25, 0, 0, -0.25), tf::Vector3(0.0, 0.0, 0.3)),
+          ros::Time::now(),"base_link", "camera"));
+
+
     }
+
 
 /*
     broadcaster.sendTransform(
