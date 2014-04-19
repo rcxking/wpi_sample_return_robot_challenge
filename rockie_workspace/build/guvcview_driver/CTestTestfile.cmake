f1814b94ff928f116ae3390fcc6e25f911c728f1
Bryant Pong <rcxking@yahoo.com>
2014-04-19 03:04:51 -0400

    Removed rockie_workspace

--- rockie_workspace/build/guvcview_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bryant/wpi_sample_return_robot_challenge/rockie_workspace/src/guvcview_driver
-# Build directory: /home/bryant/wpi_sample_return_robot_challenge/rockie_workspace/build/guvcview_driver
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
