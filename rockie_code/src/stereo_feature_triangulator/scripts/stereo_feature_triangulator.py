6cb94343cb44a2f8c50bc507cc4c2258538f6650
rockie <rockie@rockie.(none)>
2014-05-27 21:05:14 -0400

    disparity working better

--- rockie_code/src/stereo_feature_triangulator/scripts/stereo_feature_triangulator.py
+++ rockie_code/src/stereo_feature_triangulator/scripts/stereo_feature_triangulator.py
@@ -42,8 +42,7 @@ search_params = dict(checks=50)   # or pass empty dictionary
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 #camera distance is 94 mm
-#camera_dist = .094
-camera_dist = 10 
+camera_dist = .094
 
 #focal length is 579 mm
 camera_focal_length = 579
@@ -57,17 +56,20 @@ def drawMatches(gray1, kpts1, gray2, kpts2, matches):
     vis[:h2, w1:w1+w2] = gray2
     vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
 
-    #joined_img = np.hstack((gray1, gray2))
-    
     for match in matches:
         pt1 = (int(kpts1[match.queryIdx].pt[0] + gray1.shape[1]), int(kpts1[match.queryIdx].pt[1]))
         pt2  = (int(kpts2[match.trainIdx].pt[0]), int(kpts2[match.trainIdx].pt[1]))
 
+        #should check against epipolar line
         if math.fabs(pt1[1] - pt2[1]) > 20:
             continue
         
         cv2.line(vis, pt1, pt2, (0, 0, 255), 1) 
-        #cv2.line(vis, (0,0), (300, 300), (0, 0, 255), 1)
+        z = triangulate(match, kpts1, kpts2)
+
+        if z > 0:
+            cv2.circle(vis, pt1, 5, (int(z), int(z), int(z)), -1) 
+
 
     cv2.imshow('img', vis)
 
@@ -101,9 +103,10 @@ def triangulate(match, kpts1, kpts2):
     query_pt = kpts1[match.queryIdx]
     train_pt = kpts2[match.trainIdx]
 
-    disparity = query_pt.pt[0] - train_pt.pt[0]
+    disparity = math.fabs(query_pt.pt[0] - train_pt.pt[0])
 
-    Z = (camera_focal_length*camera_dist)/disparity
+    #Z = (camera_focal_length*camera_dist)/disparity
+    Z = 100/disparity
 
     return Z
 
@@ -204,15 +207,12 @@ if __name__ == '__main__':
     cam1.set(3, 640)
     cam1.set(4, 360)
     cam1.set(cv2.cv.CV_CAP_PROP_FOURCC, cv2.cv.CV_FOURCC('Y', 'U', 'Y', 'V'))
-    descriptor = cv2.SIFT(25)
+    descriptor = cv2.SIFT(50)
 
     while True:
 
         ret1, img1 = cam0.read()
         ret2, img2 = cam1.read()
-
-        #gray1 = cv2.imread('box.png',0)          # queryImage
-        #gray2 = cv2.imread('box_in_scene.png',0) # trainImage
 
         gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
         gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
