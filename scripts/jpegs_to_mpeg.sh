9a0f576d5a11f9127da86d9b7fe5f027c973d7fc
will <will@ubuntu.(none)>
2014-06-21 04:57:03 -0400

    altered mencoder script

--- scripts/jpegs_to_mpeg.sh
+++ scripts/jpegs_to_mpeg.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cd ../rockie_code/src/stereo_historian/scripts/images/right
+
 mencoder "mf://*.jpg" -mf type=jpg:fps=15 -o output.mpg -speed 1 -ofps 30 -ovc lavc -lavcopts vcodec=mpeg2video:vbitrate=2500 -oac copy -of mpeg
